3aaae2f556c4a49a69c94032cd1df5e4d942e277
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2022-06-20 13:24:16 +0100

    Add files via upload

--- bragas-cafes-project/main.py
+++ bragas-cafes-project/main.py
@@ -1,13 +1,22 @@
 import datetime as dt
 from flask import Flask, render_template
 
-application = Flask(__name__)
+
+app = Flask(__name__)
 
 year = dt.datetime.now().year
 
-@application.route("/")
+@app.route("/")
 def home():
     return render_template("index.html", current_year=year)
 
+@app.route("/about")
+def about():
+    return render_template("about.html", current_year=year)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", current_year=year)
+
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
